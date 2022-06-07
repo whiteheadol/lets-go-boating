8a618069fd6d07e8332f52fd47b1a0acadc7b13a
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-07 15:21:58 -0600

    Add pseudocode

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  // Fetch all data from API on load
+  // Hold the state of all rivers here
+
+  // define the ways in which you will change state (form inputs, favoriting...) using useState
+
   return (
     <div className="App">
+      //HomeNav
+      //HomeContainer
       <h1>Let's Go Boating!</h1>
     </div>
   );

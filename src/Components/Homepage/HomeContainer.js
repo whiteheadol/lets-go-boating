667e7e65687a82e64e64584d1991adf4aea6e524
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-07 15:22:43 -0600

    Add pseudocode

--- src/Components/Homepage/HomeContainer.js
+++ src/Components/Homepage/HomeContainer.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 
 const HomeContainer = () => {
+// Render a thumbnail for each piece in state
+// Clicking on a thumbnail will change the url to the river's dynamic id
+  return(
 
+  )
 }
 
 export default HomeContainer;

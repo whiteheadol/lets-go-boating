5c467820f1ff0625c6ada4f1db73de2494869690
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-07 17:05:52 -0600

    Add basic thumbnail elements

--- src/Components/Homepage/RiverThumbnail.js
+++ src/Components/Homepage/RiverThumbnail.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './RiverThumbnail.css'
 
 
-const RiverThumbnail = () => {
+const RiverThumbnail = ({ name, river, state, image }) => {
 // If need reference, look at tomatillos, similar functionality to the movie details page
   return(
-
+    <div className="thumbnail">
+      <h3 className="thumbnail-title" >{name}, {river}</h3>
+      <p>{state}</p>
+      <img className="thumbnail-image" src={image} />
+    </div>
   )
 }
 

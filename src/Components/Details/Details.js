86961c62343f1f27ac1770dbd556819f2fb0db9d
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-07 15:22:36 -0600

    Add pseudocode

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 
 const Details = () => {
+  // bring in the setState that allows a user to add a trip to their bucket list here
+  // allow it to change when that button is clicked
+  return(
 
+  )
 }
 
 export default Details;

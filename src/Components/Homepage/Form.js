84e1dacb8dc6aba22c4c3c9f3acfddb35c503919
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-07 15:22:40 -0600

    Add pseudocode

--- src/Components/Homepage/Form.js
+++ src/Components/Homepage/Form.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 
 const Form = () => {
+  // bring the different ways in which you can setState in as props (probably destructure for ease)
+  // invoke the set states here
+  return(
 
+  )
 }
 
 export default Form;

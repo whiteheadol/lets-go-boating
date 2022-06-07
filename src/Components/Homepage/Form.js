cd0d6cd3e9d26f33b70fe587d99f21498978aafe
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-07 15:31:10 -0600

    Add a form return paragraph

--- src/Components/Homepage/Form.js
+++ src/Components/Homepage/Form.js
@@ -5,7 +5,7 @@ const Form = () => {
   // bring the different ways in which you can setState in as props (probably destructure for ease)
   // invoke the set states here
   return(
-
+    <p>Form!</p>
   )
 }
 

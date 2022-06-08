eac778e5d79719f3343c57570cfbdd9eaef72bfc
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-08 16:44:45 -0600

    Import useEffect and reassign currentTrip upon load of details page

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Details.css';
 
-const Details = () => {
+const Details = ({ currentTrip, setCurrentTrip, setFavRivers, currentId, allRivers }) => {
   // bring in the setState that allows a user to add a trip to their bucket list here
   // allow it to change when that button is clicked
 
+  useEffect(() => {
+    fetch(`http://localhost:4000/api/v1/rivers/${currentId}`)
+      .then(response => response.json())
+      .then(data => {
+        setCurrentTrip(data)
+      })
+  }, )
 
-  // attach a link to change url to reflect a specific river id when a thumbnail is clicked on (think tomatillos)
+
   // use params to fetch the specific trip needed, and display what I want to (again, if confused, refer to tomatillos functionality)
   // add a route in the main app component to render the right thing!
 
   return(
-    <p>it's a detail!</p>
+    <div>
+      <p>{currentTrip.run_name}</p>
+    </div>
   )
 }
 

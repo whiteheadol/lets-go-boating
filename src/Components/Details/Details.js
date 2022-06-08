8ad63230d3e844bd1418c2d05cdb3491f6f3922c
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-08 16:15:53 -0600

    Add pseudocode for trip details page

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import './Details.css';
 
 const Details = () => {
   // bring in the setState that allows a user to add a trip to their bucket list here
   // allow it to change when that button is clicked
-  return(
 
+
+  // attach a link to change url to reflect a specific river id when a thumbnail is clicked on (think tomatillos)
+  // use params to fetch the specific trip needed, and display what I want to (again, if confused, refer to tomatillos functionality)
+  // add a route in the main app component to render the right thing!
+
+  return(
+    <p>it's a detail!</p>
   )
 }
 

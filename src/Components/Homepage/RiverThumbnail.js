3af64c834551b82a13eaeb1260811cc111741d0e
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-08 16:17:32 -0600

    Turn each thumbnail into a link to that trip's details

--- src/Components/Homepage/RiverThumbnail.js
+++ src/Components/Homepage/RiverThumbnail.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './RiverThumbnail.css'
+import { Link } from 'react-router-dom';
 
-
-const RiverThumbnail = ({ name, river, state, image }) => {
+const RiverThumbnail = ({ name, river, state, image, id }) => {
 // If need reference, look at tomatillos, similar functionality to the movie details page
   return(
-    <div className="thumbnail">
-      <h3 className="thumbnail-title" >{name}, {river}</h3>
-      <p>{state}</p>
-      <img className="thumbnail-image" src={image} />
-    </div>
+    <Link to={`/${id}`} className="thumbnail">
+      <div className="thumbnail">
+        <h3 className="thumbnail-title" >{name}, {river}</h3>
+        <p>{state}</p>
+        <img className="thumbnail-image" src={image} />
+      </div>
+    </Link>
   )
 }
 

cf6cc9a7e6465e5306ac548392e0356999047f2f
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-08 16:16:26 -0600

    Add very basic details page nav bar with link back home

--- src/Components/Details/DetailsNav.js
+++ src/Components/Details/DetailsNav.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './DetailsNav.css';
 
 const DetailsNav = () => {
   return(
-
+    <div className="details-nav">
+      <img src="" />
+      <h1>Let's Go Boating!</h1>
+      <Link to={`/`}><button className='home-btn'>Home</button></Link>
+      <button className='bl-btn'>Bucket List</button>
+    </div>
   )
 }
 

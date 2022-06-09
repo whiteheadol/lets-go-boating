3473b0dbe7956833a4f3e0c359787d3acc70822d
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-09 09:27:08 -0600

    Display more trip details

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -18,8 +18,16 @@ const Details = ({ currentTrip, setCurrentTrip, setFavRivers, currentId, allRive
   // add a route in the main app component to render the right thing!
 
   return(
-    <div>
-      <p>{currentTrip.run_name}</p>
+    <div className="details-container">
+      <p><strong className="trip-name">{currentTrip.run_name}, </strong>{currentTrip.river_name.join(', ')} | {currentTrip.state.join(' | ')}</p>
+      <p><strong>Difficulty: </strong>{currentTrip.difficulty.join(', ')}</p>
+      <p><strong>Permit by Lottery: </strong>{currentTrip.permit_by_lottery}</p>
+      <p><strong>Typical Season: </strong>{currentTrip.season.join(' | ')}</p>
+      <p><strong>Length in Days: </strong>{currentTrip.length_in_days.join(' | ')}</p>
+      <p><strong>Length in Miles: </strong>{currentTrip.length_in_miles}</p>
+      <p><strong>Put-in: </strong>{currentTrip.put_in}</p>
+      <p><strong>Take-out: </strong>{currentTrip.take_out}</p>
+      <p><strong>Wild and Scenic: </strong>{currentTrip.wild_and_scenic}</p>
     </div>
   )
 }

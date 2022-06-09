e27c45cb582b392011759865c49702b400b6de72
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-09 13:18:09 -0600

    Add classNames and formatting to trip thumbnails

--- src/Components/Homepage/RiverThumbnail.js
+++ src/Components/Homepage/RiverThumbnail.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './RiverThumbnail.css'
 import { Link } from 'react-router-dom';
 
-const RiverThumbnail = ({ name, river, state, image, id }) => {
+const RiverThumbnail = ({ name, river, state, image, id, season }) => {
 // If need reference, look at tomatillos, similar functionality to the movie details page
   return(
     <Link to={`/${id}`} className="thumbnail">
-      <div className="thumbnail">
-        <h3 className="thumbnail-title" >{name}, {river}</h3>
-        <p>{state}</p>
-        <img className="thumbnail-image" src={image} />
+      <div className="thumbnail-styling">
+        <div>
+          <h3 className="thumbnail-title" >{name}, {river}</h3>
+          <p>{state.join(' | ')}</p>
+          <p>{season.join(' | ')}</p>
+        </div>
+        <div>
+          <img className="thumbnail-image" src={image} />
+        </div>
       </div>
     </Link>
   )

8270ee2fdef646f7f80d442ad2e21f5b94c08c98
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-09 13:53:08 -0600

    Add classNames to BucketListThumbnails

--- src/Components/BucketList/BucketListThumbnail.js
+++ src/Components/BucketList/BucketListThumbnail.js
@@ -1,15 +1,20 @@
 import React from 'react';
-// import './RiverThumbnail.css'
+import './BucketListThumbnail.css'
 import { Link } from 'react-router-dom';
 
-const BucketListThumbnail = ({ name, river, state, image, id }) => {
+const BucketListThumbnail = ({ name, river, state, image, id, season }) => {
 // If need reference, look at tomatillos, similar functionality to the movie details page
   return(
-    <Link to={`/${id}`} className="thumbnail">
-      <div className="thumbnail">
-        <h3 className="thumbnail-title" >{name}, {river}</h3>
-        <p>{state}</p>
-        <img className="thumbnail-image" src={image} />
+    <Link to={`/${id}`} className="bl-thumbnail">
+      <div className="thumbnail-styling">
+        <div>
+          <h3 className="thumbnail-title" >{name}, {river}</h3>
+          <p>{state.join(' | ')}</p>
+          <p>{season.join(' | ')}</p>
+        </div>
+        <div>
+          <img className="thumbnail-image" src={image} />
+        </div>
       </div>
     </Link>
   )

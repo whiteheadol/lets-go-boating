49f4347142880e2621f04eb5408d8e34fb1f179b
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-10 12:00:28 -0600

    Build out better 404 page

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import './Error.css';
 
 const Error = () => {
-  // <img className="fof-image" src="" />
   return (
     <div className='fof-box'>
-      <p className='fof-message'>Hey! This page doesn't exist!</p>
-      <Link to='/' className='redirect'>Click here to go back to the main page.</Link>
+      <p className='fof-message'>Oh no! This page doesn't exist!</p>
+      <Link to='/' className='redirect'><strong className='redirect'>Click here to go back to the main page.</strong></Link>
+      <img className="fof-image" src="https://confluence-solutions.com/wp-content/uploads/2020/04/raft-flip.jpg" />
     </div>
   );
 };

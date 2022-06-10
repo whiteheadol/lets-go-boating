8509aae685a2d46a7d0699df2e07a5721dfe7763
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-10 10:59:53 -0600

    Add pseudocode

--- src/Components/Homepage/Form.js
+++ src/Components/Homepage/Form.js
@@ -22,13 +22,18 @@ const Form = ({ setSeasonText, setPermitBoolean, permitBoolean, filterRivers }) 
     }
   }
 
+// Set select value based on setSeasonText held in state
+// Look into selected attribute for options
+// Look into ability to click checkbox text also 
   return(
     <form>
       <p className="form-heading">Narrow down your trip search:</p>
       <label htmlFor="selectMonth">What month would you like to go?</label>
       <select name="selectMonth"
         id="selectMonth"
-        onChange={seasonChangeHandler}>
+        onChange={seasonChangeHandler}
+        value=""
+        >
         <option value="any">Any</option>
         <option value="Jan">January</option>
         <option value="Feb">February</option>
@@ -43,12 +48,14 @@ const Form = ({ setSeasonText, setPermitBoolean, permitBoolean, filterRivers }) 
         <option value="Nov">November</option>
         <option value="Dec">December</option>
       </select>
-      <label htmlFor="permitBoolean">Check if you're open to lottery permits</label>
+      <label htmlFor="permitBoolean">
       <input type="checkbox"
-        name="permitBoolean"
-        value="true"
-        onChange={permitBooleanHandler}
+      name="permitBoolean"
+      value="true"
+      onChange={permitBooleanHandler}
       />
+      Check if you're open to trips that require a lottery permit:
+      </label>
       <button onClick={filterRivers} type="submit" className="submit-btn" ><strong>Search!</strong></button>
     </form>
   )

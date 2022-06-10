b1fff757aa092f116a231f1575f67520756d586f
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-10 15:33:24 -0600

    Add error handling when the details fetch fails

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,15 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Details.css';
 
 const Details = ({ currentTrip, setCurrentTrip, currentId, allRivers, setFavRivers, favRivers }) => {
   // bring in the setState that allows a user to add a trip to their bucket list here
   // allow it to change when that button is clicked
+  const [detailsError, setDetailsError] = useState(false)
 
   useEffect(() => {
     fetch(`http://localhost:4000/api/v1/rivers/${currentId}`)
       .then(response => response.json())
       .then(data => {
+        setDetailsError(false)
         setCurrentTrip(data)
+      })
+      .catch(err => {
+        setDetailsError(true)
       })
   }, )
 
@@ -31,16 +36,22 @@ const Details = ({ currentTrip, setCurrentTrip, currentId, allRivers, setFavRive
     <div className="river-details">
       <div className="river-backdrop" style={{ backgroundImage: `url(${currentTrip.img})`}}>
         <div className="details-text">
-          <p><strong className="trip-name">{currentTrip.run_name}, </strong>{currentTrip.river_name.join(', ')} | {currentTrip.state.join(' | ')}</p>
-          <p><strong>Difficulty: </strong>{currentTrip.difficulty.join(', ')}</p>
-          <p><strong>Permit by Lottery: </strong>{currentTrip.permit_by_lottery}</p>
-          <p><strong>Typical Season: </strong>{currentTrip.season.join(' | ')}</p>
-          <p><strong>Length in Days: </strong>{currentTrip.length_in_days.join(' | ')}</p>
-          <p><strong>Length in Miles: </strong>{currentTrip.length_in_miles}</p>
-          <p><strong>Put-in: </strong>{currentTrip.put_in}</p>
-          <p><strong>Take-out: </strong>{currentTrip.take_out}</p>
-          <p><strong>Wild and Scenic: </strong>{currentTrip.wild_and_scenic}</p>
-          {!checkIfFav(currentTrip, favRivers) && <button className="fav-btn" onClick={addRiver}><strong>Add this trip to your bucket list!</strong></button>}
+          {detailsError && <p className="load-error">We're so sorry - there was a problem loading your data. Please try again later!</p>}
+          {!detailsError &&
+            <div>
+            <p><strong className="trip-name">{currentTrip.run_name}, </strong>{currentTrip.river_name.join(', ')} | {currentTrip.state.join(' | ')}</p>
+            <p><strong>Difficulty: </strong>{currentTrip.difficulty.join(', ')}</p>
+            <p><strong>Permit by Lottery: </strong>{currentTrip.permit_by_lottery}</p>
+            <p><strong>Typical Season: </strong>{currentTrip.season.join(' | ')}</p>
+            <p><strong>Length in Days: </strong>{currentTrip.length_in_days.join(' | ')}</p>
+            <p><strong>Length in Miles: </strong>{currentTrip.length_in_miles}</p>
+            <p><strong>Put-in: </strong>{currentTrip.put_in}</p>
+            <p><strong>Take-out: </strong>{currentTrip.take_out}</p>
+            <p><strong>Wild and Scenic: </strong>{currentTrip.wild_and_scenic}</p>
+            {!checkIfFav(currentTrip, favRivers) && <button className="fav-btn" onClick={addRiver}><strong>Add this trip to your bucket list!</strong></button>}
+            </div>
+
+          }
         </div>
       </div>
     </div>

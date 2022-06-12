1bf866f5f99d71793c58f86c05994bd30303e80d
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-06-11 20:42:32 -0600

    Remove comments and unused code

--- src/Components/Homepage/RiverThumbnail.js
+++ src/Components/Homepage/RiverThumbnail.js
@@ -3,7 +3,6 @@ import './RiverThumbnail.css'
 import { Link } from 'react-router-dom';
 
 const RiverThumbnail = ({ name, river, state, image, id, season }) => {
-// If need reference, look at tomatillos, similar functionality to the movie details page
   return(
     <Link to={`/rivers/${id}`} className="thumbnail" id={id}>
       <div className="thumbnail-styling">
